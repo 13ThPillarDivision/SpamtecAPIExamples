c50d40ea3022d740617ee50786a67ab58088d13f
Hecadoth <127027367+HecadothRU@users.noreply.github.com>
2023-04-06 11:37:25 -0400

    Update PHP7-Rest-APIRequest.php

--- PHP7-Rest-APIRequest.php
+++ PHP7-Rest-APIRequest.php
@@ -1,25 +1,22 @@
 <?php
-      header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-          $takenInput = $_GET['input'];
+if (isset($_GET['input'])) {
+    $takenInput = urlencode($_GET['input']);
 
-          if ($postId > 0) {
-              $url = 'https://endpoint.spamtec.cc/geoip?key=key&input=' . $takenInput;
-              // Replace "tool" With your desired tool & "input" with the set intake paremeter for set tool
-              $response = file_get_contents($url);
+    $url = 'https://endpoint.spamtec.cc/tool?key=key&input=' . $takenInput;
+    // Replace "tool" with your desired tool & "input" with the set intake parameter for the set tool
+    // Make sure to replace "key" with your actual API key
+    $response = file_get_contents($url);
 
-              if ($response === false) {
-                  http_response_code(500);
-                  echo json_encode(['error' => 'Error fetching data from external API']);
-              } else {
-                  echo $response;
-              }
-          } else {
-              http_response_code(400);
-              echo json_encode(['error' => 'Invalid input provided']);
-          }
-      } else {
-          http_response_code(400);
-          echo json_encode(['error' => 'input parameter is required']);
-      }
+    if ($response === false) {
+        http_response_code(500);
+        echo json_encode(['error' => 'Error fetching data from external API']);
+    } else {
+        echo $response;
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(['error' => 'Input parameter is required']);
+}
 ?>
